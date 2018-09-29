e5364748d56789408dd86c0369f4510ac081513f
Yipee-ki-yay <mopoz07@gmail.com>
2018-09-29 09:27:49 +0300

    init prodject

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <dir>
+    <header>
+      <div class="row">
+        <div class="col col-sm-12">
+          <h1>Site</h1>
+          <app-cart></app-cart>  
+        </div>
+      </div>
+    </header>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col col-sm-12">
+            <app-product></app-product>
+          </div>
+        </div>
+      </div>
+    </section>
+  </dir>
+</template>
+
+<script>
+  import AppProduct from './components/Product';
+  import AppCard from './components/Card';
+
+export default {
+  components: {
+    AppProduct,
+    AppCard
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Product title</h2>
+        <div class="price">1000</div>
+        <hr>
+        <div class="btn btn-warning">-1</div>
+        <div class="btn btn-success">+1</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
